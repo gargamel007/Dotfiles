6b95e74e16000b56125aa4ed02db288c4680e64e
gargamel007 <gargamel@m4tp.org>
2015-06-01 23:48:20 +0200

    add git configuration upon installation

--- install.sh
+++ install.sh
@@ -29,6 +29,18 @@ eval `dircolors ~/.dircolors`
 wget -P /tmp/ --no-check-certificate https://raw.githubusercontent.com/seebi/tmux-colors-solarized/master/tmuxcolors-dark.conf
 mv -f /tmp/tmuxcolors-dark.conf ~/.tmuxcolors-custom.conf
 
+# configure git
+git config --global color.ui true
+echo "please run git config --global user.name USERNAME"
+echo "please run git config --global user.email EMAIL"
+git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=7200"
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+
+
+
 # Linking files
 ln -fs $BASEDIR/tmux.conf ~/.tmux.conf
 ln -fs $BASEDIR/vimrc ~/.vimrc
